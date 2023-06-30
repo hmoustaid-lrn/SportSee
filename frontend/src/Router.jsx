c7545011a3697b9593a45e2f6612f687b71d20e0
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-06-30 15:53:51 +0200

    Router and Selection of users created

---
+++ frontend/src/Router.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home'
+
+
+function Router() {
+	return (
+		<React.StrictMode>
+			<BrowserRouter basename="/SportSee">
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/profil/:userId" />
+				</Routes>
+			</BrowserRouter>
+		</React.StrictMode>
+	)
+}
+
+export default Router
---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import './index.css'
+
+export default function Home() {
+
+    return (
+        <main>
+            <h2 class="tilte">Select USER</h2>
+            <NavLink to="profil/12">👦 Karl </NavLink>
+            <NavLink to="profil/18">👩 Cecilia </NavLink>
+        </main>
+    );
+};
