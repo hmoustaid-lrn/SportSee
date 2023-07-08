e5ba37b6b739b3334e31b0f8772b9ec581fd0ae4
Hicham Moustaid <hmoustaid.lrn@gmail.com>
2023-07-08 20:16:14 +0200

    Fetch user's mocked data

--- frontend/src/pages/Profil/index.jsx
+++ frontend/src/pages/Profil/index.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import mocked_data from "../../data/data"
+
+import { useParams } from 'react-router-dom'
+
+
+
 import './index.css';
 
 export default function Profil() {
 
-    return (
-        <main></main>
-    );
+    const { userId } = useParams();
+    console.log(userId);
+
+    let userMainData = mocked_data.USER_MAIN_DATA.filter(item => item.id === parseInt(userId));
+    let userActivity = mocked_data.USER_ACTIVITY.filter(item => item.userId === parseInt(userId));
+    let userAverageSessions = mocked_data.USER_AVERAGE_SESSIONS.filter(item => item.userId === parseInt(userId));
+    let userPerformance = mocked_data.USER_PERFORMANCE.filter(item => item.userId === parseInt(userId));
+
+
+    console.log(userMainData);
+    console.log(userActivity);
+    console.log(userAverageSessions);
+    console.log(userPerformance);
+
 };
